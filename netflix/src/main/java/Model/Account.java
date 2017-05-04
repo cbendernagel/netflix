e45ea468f7d862f2a8e247098b2313bce5663a7f
pliao <>
2017-05-03 21:30:52 -0400

    added one to one

--- netflix/src/main/java/Model/Account.java
+++ netflix/src/main/java/Model/Account.java
@@ -13,6 +13,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 /**
@@ -28,6 +29,7 @@ public class Account implements Serializable{
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private Timestamp dateOpened;
+    @OneToOne
     private AccountType acctType;
     private Customer customer;
     
